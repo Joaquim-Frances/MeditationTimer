f0694d8bb7e5004f54562e8304dd943f052f8d04
Joaquim Francès <72190276+Joaquim-Frances@users.noreply.github.com>
2020-11-22 22:47:42 +0100

    Addesd correct writting of time. Added minutes. Added basic CSS.

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 
-let time = 0;
+let seconds = 0;
+let minutes = 0;
 let isRunning = false;
 let inPause = false;
-document.getElementById("time").innerHTML= time;
+document.getElementById("seconds").innerHTML=  "0" + seconds;
+document.getElementById("minutes").innerHTML=  "0" + minutes;
+
 
 function timeCount(){
     
-    time++;
-    document.getElementById("time").innerHTML= time;
-    console.log (time);
+    seconds++;
+    if(seconds == 59+1){
+        minutes++;
+        seconds = 0;
+    }
+    printTime();
+    
 }
     
 
@@ -30,8 +37,10 @@ function startTimer(){
 function stopTimer(){
 
     clearInterval(myTime);
-    time = 0;
-    document.getElementById("time").innerHTML= time;
+    seconds = 0;
+    minutes = 0;
+    document.getElementById("seconds").innerHTML= "0" + seconds;
+    document.getElementById("minutes").innerHTML= "0" + minutes;
     isRunning = false;
 
 }
@@ -47,7 +56,26 @@ function pauseTimer(){
 
 }
 
+function printTime(){
 
+    if(seconds < 10){
+
+        document.getElementById("seconds").innerHTML= "0" + seconds; 
+    }
+    else if(seconds >= 10 && seconds <= 59){
+
+        document.getElementById("seconds").innerHTML= seconds; 
+    }
+    if(minutes >= 1 && minutes < 10){
+
+        document.getElementById("minutes").innerHTML= "0" + minutes;
+    }
+    else if(minutes >= 10){
+
+        document.getElementById("minutes").innerHTML= minutes; 
+    }
+
+}
 
 
 
