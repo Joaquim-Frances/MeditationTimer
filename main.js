dd984d7d170e44b28ab7b09a8f5ab005e31d43fb
Joaquim-Frances <quimifrances@gmail.com>
2020-11-26 10:52:35 +0100

    General Refactoring

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ let inPause = false;
 document.getElementById("seconds").innerHTML=  "0" + seconds;
 document.getElementById("minutes").innerHTML=  "0" + minutes;
 
+
+
 function addMinutes(minutesAdded){
 
     minutes += minutesAdded;
     printTime();
-
 }
+
+
 
 function minusMinutes(){
 
@@ -19,28 +22,28 @@ function minusMinutes(){
         minutes = 0;
         return;
     }
-    
     minutes--;
     printTime();
-
 }
+    
 
 
 function timeCount(){
     
     seconds--;
+
     if(seconds == 0-1){
         minutes--;
         seconds = 59;
     }
+
     printTime();
     
     if(minutes == 0 && seconds == 0){
-
         stopTimer();
         playSound();
     }
-    
+
 }
     
 
@@ -58,20 +61,18 @@ function startTimer(){
     myTime = setInterval(timeCount, 1000);
     isRunning = true;
     inPause = false;
-    
 }
 
 
+    
 function stopTimer(){
 
     if(isRunning == false){
-
         seconds = 0;
         minutes = 0;
         document.getElementById("seconds").innerHTML= "0" + seconds;
         document.getElementById("minutes").innerHTML= "0" + minutes;
         return;   
-
     }
 
     clearInterval(myTime);
@@ -84,37 +85,33 @@ function stopTimer(){
 }
 
 
+
 function pauseTimer(){
 
     if(isRunning && inPause == false){
-
         clearInterval(myTime);
         inPause = true;
     }
-
-
 }
+
+
 
 function printTime(){
 
     if(seconds < 10){
-
         document.getElementById("seconds").innerHTML= "0" + seconds; 
-    }
-    else if(seconds >= 10 && seconds <= 59){
-
+    }else if(seconds >= 10 && seconds <= 59){
         document.getElementById("seconds").innerHTML= seconds; 
     }
+
     if(minutes >= 0 && minutes < 10){
-
         document.getElementById("minutes").innerHTML= "0" + minutes;
-    }
-    else if(minutes >= 10){
-
+    }else if(minutes >= 10){
         document.getElementById("minutes").innerHTML= minutes; 
     }
-
 }
+
+
 
 function playSound(){
 
