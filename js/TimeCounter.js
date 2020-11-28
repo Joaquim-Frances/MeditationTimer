67dec08319af1b4045790f0bc27a7885c6f1d9da
Joaquim-Frances <quimifrances@gmail.com>
2020-11-28 15:28:26 +0100

    testing new ways

--- js/TimeCounter.js
+++ js/TimeCounter.js
@@ -1,7 +1,27 @@
+//let nowTime = new Date();
+let minutes = 8;
+//let addedTime = 15;
 
-let seconds = 0;
-let minutes = 0;
-let afterClick = new Date();
+function addTime(){
 
-document.getElementById("minutes").innerHTML = afterClick.toDateString;
+    minutes++;
+    document.getElementById("minutes").innerHTML = minutes;
+}
+    
+    
 
+
+function countDown(){
+
+    let nowTime = Date.now();
+    let nowMinutes = new Date(nowTime).getMinutes();
+    let nextTime = new Date(nowTime).getMinutes() + minutes;
+    
+    let timeToCount = nowMinutes - nextTime;
+
+    console.log(timeToCount);
+    
+
+}
+
+countDown();
