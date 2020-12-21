07e56cbeae195e6ea8e236cb54bf491ee53f0de7
Joaquim <quimifrances@gmail.com>
2020-12-21 22:43:21 +0100

    Adding modal mesage

--- js/screen.js
+++ js/screen.js
@@ -46,22 +46,3 @@ function testingDevice(){
         writeMesage();    
     }
 } 
-
-function writeMesage(){
-    var inDiv = document.createElement("div");
-    inDiv.setAttribute ("id", "innerDiv");
-    var pTag = document.createElement("P");
-    var mesage = document.createTextNode("If you lock your device manually, the countdown will stop.");  
-    var Check = document.createElement('input');
-    Check.type = 'checkbox';
-    var btn = document.createElement("BUTTON");
-    btn.innerHTML = "UNDERSTOOD!"; 
-    btn.setAttribute("class", "buttons");
-    inDiv.appendChild(pTag);                
-    inDiv.appendChild(Check);                
-    inDiv.appendChild(btn);                
-    pTag.appendChild(mesage);                                          
-    document.getElementById("mesage").appendChild(inDiv);
-    
-    
-}
