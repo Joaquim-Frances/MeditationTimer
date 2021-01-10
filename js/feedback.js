f51765a3354d41724dbb5df54a82cdd8386da324
Joaquim <quimifrances@gmail.com>
2021-01-10 22:26:55 +0100

    Added Paypal Donate button, added feedback first draft

---
+++ js/feedback.js
@@ -0,0 +1,31 @@
+let feedback = document.getElementById('feedback');
+let feedbackModal = document.getElementById('feedbackModal');
+let fbFlex = document.getElementById('feedbackFlex');
+let closeFeedbackModal = document.getElementById('closeFeedbackModal');
+let okFeedbackModal = document.getElementById('okFeedbackModal');
+
+
+
+
+
+feedback.addEventListener('click', function(){
+    feedbackModal.style.display = 'block';
+    
+});
+
+
+
+closeFeedbackModal.addEventListener('click', function(){
+    feedbackModal.style.display = 'none';
+    sideMenu.checked = false;
+});
+
+
+
+window.addEventListener('click', function(e){
+    if(e.target == fbFlex){
+        feedbackModal.style.display = 'none';
+        sideMenu.checked = false;
+    }
+});
+
