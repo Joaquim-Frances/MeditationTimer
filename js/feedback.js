5a41fa8117f3f556e5972bb50e964cbd4f0d78e5
Quim Frances <quim.frances@tiendeo.com>
2022-06-28 15:18:49 +0200

    Added privacy Policy web test

--- js/feedback.js
+++ js/feedback.js
@@ -1,31 +1,21 @@
-let feedback = document.getElementById('feedback');
-let feedbackModal = document.getElementById('feedbackModal');
-let fbFlex = document.getElementById('feedbackFlex');
-let closeFeedbackModal = document.getElementById('closeFeedbackModal');
-let okFeedbackModal = document.getElementById('okFeedbackModal');
+let feedback = document.getElementById("feedback");
+let feedbackModal = document.getElementById("feedbackModal");
+let fbFlex = document.getElementById("feedbackFlex");
+let closeFeedbackModal = document.getElementById("closeFeedbackModal");
+let okFeedbackModal = document.getElementById("okFeedbackModal");
 
-
-
-
-
-feedback.addEventListener('click', function(){
-    feedbackModal.style.display = 'block';
-    
+feedback.addEventListener("click", function () {
+  feedbackModal.style.display = "block";
 });
 
+closeFeedbackModal.addEventListener("click", function () {
+  feedbackModal.style.display = "none";
+  sideMenu.checked = false;
+});
 
-
-closeFeedbackModal.addEventListener('click', function(){
-    feedbackModal.style.display = 'none';
+window.addEventListener("click", function (e) {
+  if (e.target == fbFlex) {
+    feedbackModal.style.display = "none";
     sideMenu.checked = false;
+  }
 });
-
-
-
-window.addEventListener('click', function(e){
-    if(e.target == fbFlex){
-        feedbackModal.style.display = 'none';
-        sideMenu.checked = false;
-    }
-});
-
